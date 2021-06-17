becd09ff3c0b0ada78793b4a7d2d62aaa366b5ba
Kenneth Traynor <cyannotive@gmail.com>
2021-06-17 13:58:33 -0400

    feat: add initial files

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use('/users', userRouter);
+app.use('/cards', cardsRouter);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Requested resource not found' });
+});
+
+app.listen(PORT);
