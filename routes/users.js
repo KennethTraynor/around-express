ed92c2ce207d56e21e5c00952de494e214d352a5
Kenneth Traynor <cyannotive@gmail.com>
2021-06-18 13:21:09 -0400

    fix: change error message, catch file errors

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
 router.get('/', (req, res) => {
-  fs.readFile('./data/users.json', { encoding: 'utf-8' })
-    .then((data) => res.status(200).send(data));
+  fs.readFile(path.join('data', 'users.json'), { encoding: 'utf-8' })
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(404).send(err));
 });
 
 router.get('/:_id', (req, res) => {
-  fs.readFile('./data/users.json', { encoding: 'utf-8' })
+  fs.readFile(path.join('data', 'users.json'), { encoding: 'utf-8' })
     .then((data) => {
       const { _id } = req.params;
       const user = JSON.parse(data).find((u) => u._id === _id);
 
       if (!user) {
-        res.status(404).send({ message: 'User ID not found' });
+        res.status(404).send({ 'message': 'User ID not found' });
         return;
       }
 
       res.status(200).send(user);
-    });
+    })
+    .catch((err) => res.status(404).send(err));
 });
 
 module.exports = router;
