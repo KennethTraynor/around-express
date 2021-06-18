5199ecab4bd47223be081e7f7423fad9844fca42
Kenneth Traynor <cyannotive@gmail.com>
2021-06-18 13:13:26 -0400

    fix: add relative path and err catch

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
 router.get('/', (req, res) => {
-  fs.readFile('./data/cards.json', { encoding: 'utf-8' })
-    .then((data) => res.status(200).send(data));
+  fs.readFile(path.join('data', 'cards.json'), { encoding: 'utf-8' })
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(404).send(err));
 });
 
 module.exports = router;
