27d9412324b611c0a18b8f4551a4e498ccbc15e6
Kenneth Traynor <cyannotive@gmail.com>
2021-06-30 10:14:45 -0400

    feat: add mongoose and create models

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use('/users', userRouter);
 app.use('/cards', cardsRouter);
